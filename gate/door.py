ff276602a4fe738a989cb23c4709d0975972dec6
Amoki <hugo.duroux@gmail.com>
2016-02-13 17:32:18 +0100

    open door wait in a thread

--- gate/door.py
+++ gate/door.py
@@ -1,14 +1,24 @@
-import RPi.GPIO as GPIO
 import time
+from threading import Thread
+
 from gate.model import Open
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
+# import RPi.GPIO as GPIO
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(12, GPIO.OUT)
+
+
+class OpenDoor(Thread):
+    def __init__(self, lettre):
+        Thread.__init__(self)
+        self.lettre = lettre
+
+    def run(self):
+        # GPIO.output(12, GPIO.HIGH)
+        time.sleep(10)
+        # GPIO.output(12, GPIO.LOW)
 
 
 def open():
-	Open().save()  # Log the date of opening
-
-	GPIO.output(12, GPIO.HIGH)
-	time.sleep(10)
-	GPIO.output(12, GPIO.LOW)
+    Open().save()  # Log the date of opening
+    OpenDoor.start()
