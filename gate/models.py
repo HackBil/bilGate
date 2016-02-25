dc437e3cce4d27ceed7496e96d3dc3c7fabde5bf
Amoki <hugo.duroux@gmail.com>
2016-02-25 16:45:15 +0100

    remove restriction on code

--- gate/models.py
+++ gate/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import RegexValidator
 
 
 class Open(models.Model):
@@ -7,4 +6,4 @@ class Open(models.Model):
 
 
 class Code(models.Model):
-    code = models.CharField(max_length=5, unique=True, validators=[RegexValidator(regex='^.{5}$', message='Le code doit faire 5 caractères')])
+    code = models.CharField(unique=True)
