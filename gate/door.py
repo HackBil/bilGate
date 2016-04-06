3e7c9f6fbfc2e8696a7ed2580633398738a665ba
Hugo Duroux <hugoduroux@gmail.com>
2016-04-06 14:55:20 +0200

    Update door.py

--- gate/door.py
+++ gate/door.py
@@ -4,15 +4,17 @@ from threading import Thread
 from gate.models import Open
 
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
-
+GPIO.output(12, GPIO.LOW) # Set default as low
 
 class OpenDoor(Thread):
     def run(self):
         GPIO.output(12, GPIO.HIGH)
         time.sleep(10)
         GPIO.output(12, GPIO.LOW)
+        GPIO.cleanup()
 
 
 def open():
