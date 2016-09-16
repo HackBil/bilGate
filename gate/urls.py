94a3ab2fe13b52c4dd78e304c493a89f5ac89e96
Guillaume Besson <guillaume@besson.co>
2016-09-16 16:49:05 +0200

    Added twilio reponse API

--- gate/urls.py
+++ gate/urls.py
@@ -4,5 +4,6 @@ from gate import views
 
 urlpatterns = [
     url(r'^open$', views.open),
+    url(r'^twilio$', views.twilioResponse),
     url(r'^$', views.index),
 ]
--- gate/views.py
+++ gate/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import twilio.twiml
+import time
 
 from gate.models import Code
 import gate.door as door
@@ -29,3 +31,19 @@ def open(request):
         return HttpResponse('Unauthorized', status=401)
     door.open()
     return HttpResponse('OK', status=200)
+
+@csrf_exempt
+def twilioResponse(request):
+    resp = twilio.twiml.Response()
+    resp.say("Bienvenue chez BoostInLyon !", language="fr-FR", voice="alice")
+
+    hour = time.localtime().tm_hour
+    day_of_week = time.localtime().tm_wday
+    if hour > 8 and hour < 19 and day_of_week < 5:
+        resp.play(digits="www#")
+        resp.say("Vous pouvez maintenant entrer, veuillez sonnez à l'interphone sur la première porte à gauche, la porte sera ouverte automatiquement. Les locaux sont ensuite au premier étage, vous n'avez qu'a pousser la porte.", language="fr-FR", voice="alice")
+    else:
+        resp.say("Une sonnerie a été déclanchée dans les locaux, quelqu'un va venir vous ouvrir", language="fr-FR", voice="alice")
+
+
+    return HttpResponse(str(resp), status=200)
